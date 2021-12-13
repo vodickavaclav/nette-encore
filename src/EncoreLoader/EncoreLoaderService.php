3f55e5befa2c39ef09a54c88bff1a9691657209d
Václav Vodička <vaclav.vodicka@nms.cz>
2021-12-13 15:47:49 +0100

    Check duplicity

--- src/EncoreLoader/EncoreLoaderService.php
+++ src/EncoreLoader/EncoreLoaderService.php
@@ -11,6 +11,9 @@ class EncoreLoaderService
 	/** @var string */
 	private $defaultEntry;
 
+	/** @var string[] */
+	private $files = [];
+
 	public function __construct(array $encoreConfig)
 	{
 		$this->outDir = $encoreConfig['outDir'];
@@ -60,7 +63,21 @@ class EncoreLoaderService
 		if (!isset($entryPoints[$entry][$type])) {
 			return [];
 		}
-		return $entryPoints[$entry][$type];
+
+		return $this->checkDuplicity($entryPoints[$entry][$type]);
+	}
+
+	private function checkDuplicity(array $entryPoints)
+	{
+		foreach ($entryPoints as $key => $entryPoint) {
+			if (in_array($entryPoint, $this->files, true)) {
+				unset($entryPoints[$key]);
+			} else {
+				$this->files[] = $entryPoint;
+			}
+		}
+
+		return $entryPoints;
 	}
 
 	public function setOutDir(string $outDir): void
